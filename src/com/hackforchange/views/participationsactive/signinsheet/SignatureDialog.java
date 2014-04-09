e7fd9d72a4c1f448a8397934230e563e37660e0d
Raj Rao <neeraj2608@gmail.com>
2014-04-09 08:51:20 +0100

    Moved to RelativeLayout for some activities to fix the view on smaller screen devices (tested with my Moto G).

--- src/com/hackforchange/views/participationsactive/signinsheet/SignatureDialog.java
+++ src/com/hackforchange/views/participationsactive/signinsheet/SignatureDialog.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
@@ -20,7 +21,7 @@ public class SignatureDialog extends SherlockDialogFragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
           Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.dialog_signature, container, false);
-
+    
     saveButton = (StyledButton) view.findViewById(R.id.submitbutton);
     clearButton = (StyledButton) view.findViewById(R.id.clearButton);
     signatureView = (SignatureView) view.findViewById(R.id.signatureview);
@@ -52,5 +53,17 @@ public class SignatureDialog extends SherlockDialogFragment {
 
     return view;
   }
+  
+  @Override
+  public void onStart() {
+    super.onStart();
+
+    if (getDialog() == null) {
+      return;
+    }
+    
+    getDialog().getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+  }
+   
 
 }
